ffa038f22744e2183a95aa3a5fc19f1cabccb3e3
Dave Gardner <dave@mpdconsulting.co.uk>
2013-05-02 09:24:26 +0100

    Formatted

--- heightmap.go
+++ heightmap.go
@@ -1,6 +1,6 @@
 package heightmap
 
-import(
+import (
 	"fmt"
 )
 
@@ -43,23 +43,23 @@ func (self *HeightMap) AddPerlinNoise(f float64) {
 func (self *HeightMap) Perturb(f, d float64) {
 	temp := make([][]float64, self.dimension)
 	var u, v int
- 	for i := 0; i < self.dimension; i++ {
+	for i := 0; i < self.dimension; i++ {
 		temp[i] = make([]float64, self.dimension)
-  		for j := 0; j < self.dimension; j++ {
-   			u = i + int(self.perlin.Noise(f * float64(i) / float64(self.dimension), f * float64(j) / float64(self.dimension), 0.0) * d);
-   			v = j + int(self.perlin.Noise(f * float64(i) / float64(self.dimension), f * float64(j) / float64(self.dimension), 1.0) * d);
-   			if u < 0 {
-			 	u = 0
+		for j := 0; j < self.dimension; j++ {
+			u = i + int(self.perlin.Noise(f*float64(i)/float64(self.dimension), f*float64(j)/float64(self.dimension), 0.0)*d)
+			v = j + int(self.perlin.Noise(f*float64(i)/float64(self.dimension), f*float64(j)/float64(self.dimension), 1.0)*d)
+			if u < 0 {
+				u = 0
 			} else if u >= self.dimension {
 				u = self.dimension - 1
 			}
-   			if v < 0 {
-			 	v = 0
+			if v < 0 {
+				v = 0
 			} else if v >= self.dimension {
 				v = self.dimension - 1
 			}
-   			temp[i][j] = self.Heights[u][v]
-  		}
- 	}
- 	self.Heights = temp
+			temp[i][j] = self.Heights[u][v]
+		}
+	}
+	self.Heights = temp
 }
