58cbacfed6440aa700659cceaef7538ec3b64780
Dave Gardner <dave@mpdconsulting.co.uk>
2013-05-10 11:47:44 +0100

    Adding erosion and allowing a scale factor on the noise function

--- heightmap.go
+++ heightmap.go
@@ -2,6 +2,7 @@ package heightmap
 
 import (
 	"fmt"
+	"math"
 )
 
 type HeightMap struct {
@@ -32,10 +33,10 @@ func (self *HeightMap) String() string {
 	return s
 }
 
-func (self *HeightMap) AddPerlinNoise(f float64) {
+func (self *HeightMap) AddPerlinNoise(f, scale float64) {
 	for i := 0; i < self.dimension; i++ {
 		for j := 0; j < self.dimension; j++ {
-			self.Heights[i][j] += self.perlin.Noise(f*float64(i)/float64(self.dimension), f*float64(j)/float64(self.dimension), 0.0)
+			self.Heights[i][j] += scale * self.perlin.Noise(f*float64(i)/float64(self.dimension), f*float64(j)/float64(self.dimension), 0.0)
 		}
 	}
 }
@@ -62,4 +63,45 @@ func (self *HeightMap) Perturb(f, d float64) {
 		}
 	}
 	self.Heights = temp
+}
+
+func (self *HeightMap) Erode(smoothness float64) {
+	for i := 1; i < self.dimension-1; i++ {
+		for j := 1; j < self.dimension-1; j++ {
+   			d_max := 0.0
+   			match := make([]int, 2)
+   			for u := -1; u <= 1; u++ {
+    			for v := -1; v <= 1; v++ {
+     				if math.Abs(float64(u)) + math.Abs(float64(v)) > 0 {
+						d_i := self.Heights[i][j] - self.Heights[i + u][j + v]
+      					if d_i > d_max {
+       						d_max = d_i
+       						match[0] = u
+							match[1] = v
+      					}
+     				}
+    			}
+   			}
+
+   			if 0 < d_max && d_max <= (smoothness / float64(self.dimension)) {
+    			d_h := 0.5 * d_max
+    			self.Heights[i][j] -= d_h
+    			self.Heights[i + match[0]][j + match[1]] += d_h
+   			}
+  		}
+ 	}
+}
+
+func (self *HeightMap) Smoothen() {
+	for i := 1; i < self.dimension-1; i++ {
+		for j := 1; j < self.dimension-1; j++ {
+   			total := 0.0
+   			for u := -1; u <= 1; u++ {
+    			for v := -1; v <= 1; v++ {
+     				total += self.Heights[i+u][j+v]
+    			}
+   			}
+   			self.Heights[i][j] = total / 9.0
+  		}
+ 	}
 }
